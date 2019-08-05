8e87890d033764cf544be51db72715be280dc669
vanpersietyo <barcelonitas.adhyt@gmail.com>
2019-08-05 15:05:25 +0700

    commit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1131,66 +1131,6 @@ class Admin extends CI_Controller {
 		}
 	}
 
-	function load_class($class, $directory = 'application/libraries', $param = NULL)
-	{
-		static $_classes = array();
-
-		// Does the class exist? If so, we're done...
-		if (isset($_classes[$class]))
-		{
-			return $_classes[$class];
-		}
-
-		$name = FALSE;
-
-		// Look for the class first in the local application/libraries folder
-		// then in the native system/libraries folder
-		foreach (array(APPPATH, BASEPATH) as $path)
-		{
-			if (file_exists($path.$directory.'/'.$class.'.php'))
-			{
-//				$name = 'CI_'.$class;
-
-				if (class_exists($name, FALSE) === FALSE)
-				{
-					var_dump($path.$directory.'/'.$class.'.php');
-					require_once($path.$directory.'/'.$class.'.php');
-				}
-
-				break;
-			}
-		}
-
-		// Is the request a class extension? If so we load it too
-		if (file_exists(APPPATH.$directory.'/'.config_item('subclass_prefix').$class.'.php'))
-		{
-			$name = config_item('subclass_prefix').$class;
-
-			if (class_exists($name, FALSE) === FALSE)
-			{
-				require_once(APPPATH.$directory.'/'.$name.'.php');
-			}
-		}
-
-		// Did we find the class?
-		if ($name === FALSE)
-		{
-			// Note: We use exit() rather than show_error() in order to avoid a
-			// self-referencing loop with the Exceptions class
-			set_status_header(503);
-			echo 'Unable to locate the specified class: '.$class.'.php';
-			exit(5); // EXIT_UNK_CLASS
-		}
-
-		// Keep track of what we just loaded
-		is_loaded($class);
-
-		$_classes[$class] = isset($param)
-			? new $name($param)
-			: new $name();
-		var_dump($_classes[$class]) ;
-	}
-
 
 }
 /* End of file Admin.php */
